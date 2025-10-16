ef639ccffad03be99101178a016f82a724c818ce
KeystoneDigitalHoldings <admin@keystonedigitalholdings.com>
2025-10-16 02:03:07 -0700

    Create terms page.tsx

---
+++ app/(routes)/terms/page.tsx
@@ -0,0 +1,8 @@
+export default function TermsPage() {
+  return (
+    <section className="prose max-w-none">
+      <h1>Terms of Service</h1>
+      <p>By using HotSpringMaps you agree to our community guidelines.</p>
+    </section>
+  );
+}
