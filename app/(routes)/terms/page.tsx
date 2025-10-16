22a8560ef616adada3a2f81587c51a976d74e165
KeystoneDigitalHoldings <admin@keystonedigitalholdings.com>
2025-10-16 02:00:03 -0700

    Delete app/(routes)/terms directory

--- app/(routes)/terms/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TermsPage() {
-  return (
-    <section className="prose max-w-none">
-      <h1>Terms of Service</h1>
-      <p>By using HotSpringMaps you agree to our community guidelines.</p>
-    </section>
-  );
-}
